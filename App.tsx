baf6ef045cd6c03dd43ffaeb5dfc02f09c0b8aaa
Pcode13 <veer.pragati99@gmail.com>
2025-01-06 17:25:18 +0530

    App

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log("Hello")
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello App</Text>
       <StatusBar style="auto" />
     </View>
   );
